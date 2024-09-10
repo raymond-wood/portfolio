7849806de574ab7e38b0d0e252dd8cbf77b30a9e
raymond-wood <87679808+raymond-wood@users.noreply.github.com>
2024-09-10 20:31:03 +0000

    Added @Angular/Material

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolBar } from '@angular/material/MatToolBar'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolBar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
